6bbdcbcd481fbe38742d17a4f48f386fc62ddf25
PAN <624305162@qq.com>
2020-05-28 19:36:35 +0800

    增加模板引擎

--- src/main/java/controller/ShiftController.java
+++ src/main/java/controller/ShiftController.java
@@ -1,29 +1,37 @@
 package controller;
 
 import Service.ShiftInterface;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import model.ShiftInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ShiftController {
     private static final Logger log = LoggerFactory.getLogger(ShiftController.class);
 
     @Autowired
     private ShiftInterface shiftInterface;
 
-    @RequestMapping("/showShift")
-    public String showShift() {
-        //展示开班信息
-        List<ShiftInfo> shiftInfoList = shiftInterface.findAll();
-        log.info("====="+shiftInfoList.size());
+    @RequestMapping("/index")
+    public String index() {
 
-        return "改一下111";
+        return "index";
     }
+
+    @RequestMapping("/showShift")
+    @ResponseBody
+    public List showShift(){
+        //显示开班信息
+        List<ShiftInfo> shiftInfoList = shiftInterface.findAll();
+        return shiftInfoList;
+    }
+
 }
